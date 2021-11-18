56e699fbdab6c89c7c9393cc36b6c947931c2696
ulisses malanski <malanskiwork@gmail.com>
2021-11-18 20:30:55 -0300

    Add User Button to aside, refactor script

--- js/script.js
+++ js/script.js
@@ -27,27 +27,14 @@ let myButton = document.querySelector('button.user-button');
 let myAsideUser = document.querySelector('h4');
 
 function defineNomeUsuario() {
-  let meuNome = prompt('Say your name or type anything please.');
+  let meuNome = prompt('Type your name or type nickname please.');
   localStorage.setItem('nome', meuNome);
-  myAsideUser.textContent = 'and try me, ' + meuNome;
+  myAsideUser.textContent = 'Welcome to the Pokéloja, ' + meuNome;
 }
 
 if(!localStorage.getItem('nome')) {
   defineNomeUsuario();
 } else {
   let nomeGuardado = localStorage.getItem('nome');
-  myAsideUser.textContent = 'and try me, ' + nomeGuardado;
-}
-
-function defineNomeUsuario() {
-  let meuNome = prompt('Say your name or type anything please.');
-  if(!meuNome || meuNome === null) {
-    defineNomeUsuario();
-  } else {
-    localStorage.setItem('nome', meuNome);
-    myAsideUser.innerHTML = 'and try me, ' + meuNome;
-  }
-}
-
-myButton.onclick = function() { defineNomeUsuario();
+  myAsideUser.textContent = 'Welcome to the Pokéloja, ' + nomeGuardado;
 }
